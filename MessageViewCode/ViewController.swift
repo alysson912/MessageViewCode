51801ed5766973b29571584aebf14f377f847983
Alysson Menezes <almenezes912@gmail.com>
2022-06-07 16:48:24 -0300

    Initial Commit

---
+++ MessageViewCode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MessageViewCode
+//
+//  Created by ALYSSON MENEZES on 07/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
